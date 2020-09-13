f5078b30aa9b731ad5802934b1cabbcc9d3869a4
Mike He <mikehe12@gmail.com>
2020-09-12 22:24:17 -0700

    Added referencing and links from unit test to source files

---
+++ Doc.Test/StreamPipelineReaderSelfParseTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Doc.Test
+{
+	public class StreamPipelineReaderSelfParseTest
+	{
+		readonly static string filename = Path.Combine(TestSamples.Folder, "StreamPipelineReader.cs");
+
+		readonly ITestOutputHelper output;
+
+		public StreamPipelineReaderSelfParseTest(ITestOutputHelper output)
+		{
+			this.output = output;
+		}
+
+		[Fact]
+		public void CanFindCorrectFile()
+		{
+			var file = new FileInfo(filename);
+
+			var reader = file.OpenText();
+
+		}
+
+	}
+}
---
+++ Doc.Test/TestSamples/TestSamples.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Doc.Test
+{
+	public static class TestSamples
+	{
+		const string SamplesFolder = "TestSamples";
+
+		public static string Folder { get; } = Path.Combine(Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location), SamplesFolder);
+	}
+}
