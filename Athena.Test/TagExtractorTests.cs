29bcf971d4fbd88567a01664f908466f27e121cf
Mike He <mikehe12@gmail.com>
2020-11-17 22:17:23 -0800

    Added Tag extractor and tests

---
+++ Athena.Test/TagExtractorTests.cs
@@ -0,0 +1,55 @@
+﻿using Athena.Blocks;
+using System;
+using System.Buffers;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Athena.Test
+{
+
+	public class TagExtractorTests
+	{
+		readonly ITestOutputHelper output;
+		TagExtractor tagExtractor;
+
+		public TagExtractorTests(ITestOutputHelper output)
+		{
+			this.output = output;
+			tagExtractor = new TagExtractor();
+		}
+
+		[Theory]
+		[InlineData("")]
+		public void GivenNoTags_ReturnsFalse(string testLine)
+		{
+			var sequence = new ReadOnlySequence<byte>(Encoding.UTF8.GetBytes(testLine));
+
+			var (tagFound, tags) = tagExtractor.TryGetTags(sequence);
+
+			Assert.False(tagFound);
+			Assert.Equal(default, tags);
+		}
+
+
+		[Theory]
+		[InlineData("abcd.efjk.lmno")]
+		[InlineData(".abcd.efjk.lmno")]
+		[InlineData(". abcd. efjk.lmno")]
+		[InlineData("..abcd.efjk.	lmno	.")]
+		public void GivenTags_FindsTags(string testLine)
+		{
+			var sequence = new ReadOnlySequence<byte>(Encoding.UTF8.GetBytes(testLine));
+
+			var (tagFound, tags) = tagExtractor.TryGetTags(sequence);
+
+			Assert.True(tagFound);
+			var expectedTags = new string[]{ "abcd", "efjk", "lmno" };
+			Assert.Equal(expectedTags, tags);
+		}
+
+	}
+}
---
+++ Athena/Blocks/TagExtractor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Buffers;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Athena.Blocks
+{
+	public sealed class TagExtractor
+	{
+		const char TagDelimiter = '.';
+
+		public (bool, string[]) TryGetTags(in ReadOnlySequence<byte> source)
+		{
+			var subTags = Encoding.UTF8.GetString(source).Trim().Split(TagDelimiter, StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+
+			if (subTags.Length <= 0) return default;
+			return (true, subTags);
+		}
+	}
+}
