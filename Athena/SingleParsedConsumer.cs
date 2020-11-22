bf9e1214ce2bdfb6935204be994a2b8ec0ae363f
Mike He <mikehe12@gmail.com>
2020-11-21 20:53:32 -0800

    Added alternate usage in conusmer with tuple returns and no nullable SequencePosition

--- Athena/SingleParsedConsumer.cs
+++ Athena/SingleParsedConsumer.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Athena.DataTypes;
+using System;
 using System.Buffers;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,12 +11,30 @@ namespace Athena
 	public sealed class SingleParsedConsumer<T> : IBufferConsumer
 	{
 		readonly IBufferParser<T> parser;
+		readonly IParserBlock<FileBlockBuffer, T, int> parserBlock;
 		readonly Action<T> consumer;
 
 		public SingleParsedConsumer(IBufferParser<T> parser, Action<T> consumer)
 		{
 			this.parser = parser;
 			this.consumer = consumer;
+		}
+
+
+		public SequencePosition TryConsume(ref FileBlockBuffer fileBlock, ref int lineNumber)
+		{
+			var buffer = fileBlock.Buffer;
+			SequencePosition parsed = buffer.Start;
+
+			while (parserBlock.Parse(fileBlock, ref lineNumber) is (true, var output))
+			{
+				consumer(output);
+
+				// Advance past the parsed section
+				//parsed = buffer.GetPosition(1, parsedSection);
+			}
+
+			return parsed;
 		}
 
 		public SequencePosition TryConsume(ref ReadOnlySequence<byte> buffer)
